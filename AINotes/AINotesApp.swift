d06284a3625aa6ca6781b84e3f53aa7899d5598d
awkoombe <awkoombe.so@gmail.com>
2025-07-11 11:47:19 +0300

    Initial Commit

---
+++ AINotes/AINotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AINotesApp.swift
+//  AINotes
+//
+//  Created by Nor Abdirahman on 11/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct AINotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AINotesTests/AINotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  AINotesTests.swift
+//  AINotesTests
+//
+//  Created by Nor Abdirahman on 11/07/2025.
+//
+
+import Testing
+@testable import AINotes
+
+struct AINotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
